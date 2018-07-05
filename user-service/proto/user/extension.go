46afbc626c31fc381eac1e8cd5f3c90ebddb6d40
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2018-07-05 18:59:59 +0530

    extension check

--- user-service/proto/user/extension.go
+++ user-service/proto/user/extension.go
@@ -2,13 +2,10 @@ package go_micro_srv_user
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
+	"github.com/pborman/uuid"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uid, err := uuid.NewV4()
-	if err != nil {
-		return err
-	}
-	return scope.SetColumn("Id", uid.String())
+	uid := uuid.New()
+	return scope.SetColumn("Id", uid)
 }
